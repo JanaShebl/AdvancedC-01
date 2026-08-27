ed273624c7448797dd67e24e74a227d06d0f921b
JanaShebl <janamohamed195200@gmail.com>
2026-08-27 21:27:37 +0300

    Q1

--- AdvancedC#01/Program.cs
+++ AdvancedC#01/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            // Q1: What is a generic class? Why use generics?
+            // generic class => allows you to write the class without declare the type , type is speccified when creating an object
+            // why? generics provide better performance , can be reused , provide clean code and type dafety(no boxing & unboxing)
+
+            #endregion
         }
     }
 }
